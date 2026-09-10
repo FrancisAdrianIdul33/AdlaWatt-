a966eeae88baf34162a7b567e5192dd42a68d759
FrancisAdrianIdul33 <francis.idul78@gmail.com>
2026-09-10 21:56:23 +0800

    Removed gradient style in colors.ts

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -35,10 +35,6 @@ export const Colors = {
     warning: "#F59E0B",
     error: "#EF4444",
   },
-  gradient: {
-    navBarStart: "#f5f9f5",
-    navBarEnd: "#61e290",
-  },
    glass: {
     white: "rgba(255, 255, 255, 0.50)",
     whiteStrong: "rgba(255, 255, 255, 0.78)",
